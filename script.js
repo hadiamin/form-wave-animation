e7cbeb93b03db0313b185ce32bc349ca4d40e9ec
aminhad <amin.hadi@amalitech.org>
2021-08-03 13:33:31 +0000

    style: made changes to transition time

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ labels.forEach(label => {
     label.innerHTML = label.innerText
         .split('')
         .map((letter, index) => `<span 
-        style="transition:${index * 50}ms">${letter}</span>`)
+        style="transition:${index * 100}ms">${letter}</span>`)
         .join('')
 })
